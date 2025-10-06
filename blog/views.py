78a8fcb4a4e4e3a2295744f4ae23865ee347c333
madiyarbase@gmail.com <arman1305clash@gmail.com>
2025-10-06 16:16:01 +0500

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
+from django.utils import timezone
 from django.shortcuts import render
+from .models import Post
+
+
+Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
